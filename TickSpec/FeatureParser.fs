42e37e150c306c09c6b020959caed722f26b1f73
Milos Chaloupka <milos.chaloupka@ncr.com>
2017-10-20 18:09:14 +0200

    First draft of smarter examples combinations.

--- TickSpec/FeatureParser.fs
+++ TickSpec/FeatureParser.fs
@@ -5,20 +5,52 @@ open TickSpec.LineParser
 open TickSpec.BlockParser
 
 /// Computes combinations of table values
-let internal computeCombinations (tables:Table []) =
+let internal computeCombinations (tables:Table []):((string*string) list list) =
+    // TODO: This method needs to be rewritten
+    // The tables should have also header outside, so it will
+    // be easy to decide which tables to join, which ones to union.
+    // The current implementation does the join. It is also needed
+    // to add the ability to union two tables. The join needs also to
+    // support the join over more than one column.
+    
+    let addCellToExampleRow row (header,value) =
+        let found =
+            row
+            |> List.tryFind (fun (h,_) -> header=h)
+
+        match found with
+        | Some (_,v) ->
+            if v = value then row
+            else row
+        | None -> (header,value) :: row
+
+    let mergeExampleRows row1 row2 =
+        row1
+        |> List.fold addCellToExampleRow row2
+
+    /// Computes all combinations
+    let rec combinations = function
+    | [] -> [[]]
+    | table :: tss ->
+        [ for row1 in table do
+            for row2 in combinations tss ->
+                mergeExampleRows row1 row2 ]
+
     let values = 
-        tables 
+        tables
         |> Seq.map (fun table ->
-            table.Rows |> Array.map (fun row ->
+            table.Rows 
+            |> Array.map (fun row ->
                 row
                 |> Array.mapi (fun i col ->
-                    table.Header.[i],col
-                )
-            )
+                    table.Header.[i],col)
+                |> Array.toList)
+            |> Array.toList
         )
         |> Seq.toList
-    values |> List.combinations
-    
+
+    values |> combinations
+
 /// Replace line with specified named values
 let internal replaceLine (xs:seq<string * string>) (scenario,n,tags,line,step) =
     let replace s =
@@ -80,11 +112,11 @@ let parseFeature (lines:string[]) =
                 // Execute each combination
                 combinations |> Seq.mapi (fun i combination ->
                     let name = sprintf "%s(%d)" name i
-                    let combination = Seq.concat combination |> Seq.toArray
+                    let combination = combination |> Seq.toArray
                     let steps =
                         Seq.append background steps
                         |> Seq.map (replaceLine combination)
-                        |> Seq.map toStep 
+                        |> Seq.map toStep
                         |> Seq.toArray
                     { Name=name; Tags=tags; Steps=steps; Parameters=combination }
                 )
